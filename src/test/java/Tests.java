b04cb3316ed8d8ae287ef524aade5ddfe037893f
jxkume <jhonr@metropolia.fi>
2023-09-18 17:52:12 +0300

    ..dsaf

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -5,6 +5,7 @@ public class Tests {
 
     @Test
     public void testCalculateDistance() {
+
         double distance = Main.calculateDistance(1.0, 2.0, 4.0, 6.0);
         assertEquals(5.0, distance, 0.001); // Tolerate small differences due to double precision
     }
