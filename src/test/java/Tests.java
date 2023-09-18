d529c4c208960abb06ed9b1e2f3d6179f00509e8
jxkume <jhonr@metropolia.fi>
2023-09-18 17:19:51 +0300

    ..

---
+++ src/test/java/Tests.java
@@ -0,0 +1,13 @@
+public class Tests {
+
+    public static double calculateDistance(double x1, double y1, double x2, double y2) {
+        double deltaX = x2 - x1;
+        double deltaY = y2 - y1;
+        return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    }
+
+    public static void main(String[] args) {
+        double distance = calculateDistance(1.0, 2.0, 4.0, 6.0);
+        System.out.println("Distance: " + distance);
+    }
+}
